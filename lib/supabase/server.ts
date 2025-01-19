1e5505195933e15f211bcf9bb8762e25e060c807
Hridya Agrawal <hridayahoney@gmail.com>
2025-01-19 14:26:21 +0000

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,8 +1,8 @@
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
-export function createServerClien() {
-  const cookieStore = cookies();
+export async function createServerClien() {
+  const cookieStore = await cookies();
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
