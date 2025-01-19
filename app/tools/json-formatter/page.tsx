020145a77a121698bc982b9447495d424d3b78ef
Hridya Agrawal <hridayahoney@gmail.com>
2025-01-19 14:37:21 +0000

    Update page.tsx

--- app/tools/json-formatter/page.tsx
+++ app/tools/json-formatter/page.tsx
@@ -1,14 +1,9 @@
-import type { Metadata } from 'next';
+'use client'
 import dynamic from 'next/dynamic';
 const JsonFormatter = dynamic(
   () => import('@/components/tools/jsonformatter'),
   { ssr: false }
 );
-
-export const metadata: Metadata = {
-  title: 'JSON Toolkit | DevForge',
-  description: 'Format, validate, and beautify your JSON data with ease',
-};
 
 export default function JsonFormatterPage() {
   return <JsonFormatter />;
