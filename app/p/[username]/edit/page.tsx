6801dc65057ff154108af120beb915a1daca1d45
Hridya Agrawal <hridayahoney@gmail.com>
2025-01-19 14:11:54 +0000

    Update page.tsx

--- app/p/[username]/edit/page.tsx
+++ app/p/[username]/edit/page.tsx
@@ -1,16 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Suspense } from 'react';
 import EditProfileClient from '@/components/tools/socialbio/editprofile';
 
-interface PageProps {
-  params: { username: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-}
-
-export default async function EditProfilePage({
-  params,
-  searchParams,
-}: PageProps) {
+export default async function EditProfilePage({ params }: any) {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <EditProfileClient username={params.username} />
